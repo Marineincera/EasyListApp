b3f79157c1277591036054f3a7052b766f396f60
Marine Incera <marine.incera@gmail.com>
2021-06-20 23:11:24 +0200

    build: homepage component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
