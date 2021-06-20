40f69ecaaf65bf01e195d960701146da285d25c5
Marine Incera <marine.incera@gmail.com>
2021-06-21 01:22:56 +0200

    signup-page: router added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './pages/homepage/homepage.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
 
 const routes: Routes = [
   { path: "", component: HomepageComponent },
   { path: "homepage", component: HomepageComponent },
+  { path: "signup", component: SignupPageComponent },
+
 ];
 
 @NgModule({
