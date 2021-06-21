8fce28d2fdc8257b2552775f883f5cbb136441c7
Marine Incera <marine.incera@gmail.com>
2021-06-21 02:56:31 +0200

    signup-page: signup form structure done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
-import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     HomepageComponent,
-    SignupPageComponent
+    SignupPageComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/signup-page/signup-page.component.ts
+++ src/app/pages/signup-page/signup-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators, FormGroup } from '@angular/forms';
+import { passwordValidator } from './password-validator';
+
+
 
 @Component({
   selector: 'app-signup-page',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-page.component.scss']
 })
 export class SignupPageComponent implements OnInit {
+  signupForm = new FormGroup({
+    pseudo: new FormControl('', [
+      Validators.required
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      passwordValidator
+    ])
+});
+
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(){
+    let userToSignup = {
+      pseudo : this.signupForm.value.pseudo, 
+      email : this.signupForm.value.email,
+      password : this.signupForm.value.password
+    }
+    console.log(userToSignup);
+    return userToSignup;
   }
 
 }
